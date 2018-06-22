449074ecee829ccaf478a0cd07fde1ec23c5081b
Seamus Kirby - Intern <Seamus.Kirby@bentley.com>
2018-06-22 16:35:23 -0400

    Restructured slightly to fix react warnings (table rows not inside <thead> and <tbody> tags)

--- src/components/body/SideNav.tsx
+++ src/components/body/SideNav.tsx
@@ -5,26 +5,32 @@ function SideNav(props: any) {
     <nav className="SideNav">
       <br/>
       <table className="nav-vertical">
-        <th>Files</th>
-        <th>Upload</th>
-        <th><a href="#" onClick={props.resetFiles}>Delete</a></th>
-        {
-        props.files.map((f: any) =>
-          <tr key={f.name}>
-            <td><a href="#"
-              onClick={props.selectFile.bind(props.container, f.name)}>
-              {f.name}
-            </a></td>
-            <td><a href="#"
-              onClick={props.uploadFile.bind(props.container, f.name)}>
-              <img src={window.location.origin + "/resources/icons/upload.png"}/>
-            </a></td>
-            <td><a href="#"
-              onClick={props.deleteFile.bind(props.container, f.name)}>
-              <img src={window.location.origin + "/resources/icons/delete.png"}/>
-            </a></td>
-          </tr>)
-        }
+        <thead>
+          <tr>
+            <th>Files</th>
+            <th>Upload</th>
+            <th><a href="#" onClick={props.resetFiles}>Delete</a></th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+          props.files.map((f: any) =>
+            <tr key={f.name}>
+              <td><a href="#"
+                onClick={props.selectFile.bind(props.container, f.name)}>
+                {f.name}
+              </a></td>
+              <td><a href="#"
+                onClick={props.uploadFile.bind(props.container, f.name)}>
+                <img src={window.location.origin + "/resources/icons/upload.png"}/>
+              </a></td>
+              <td><a href="#"
+                onClick={props.deleteFile.bind(props.container, f.name)}>
+                <img src={window.location.origin + "/resources/icons/delete.png"}/>
+              </a></td>
+            </tr>)
+          }
+        </tbody>
       </table>
     </nav>
   );
